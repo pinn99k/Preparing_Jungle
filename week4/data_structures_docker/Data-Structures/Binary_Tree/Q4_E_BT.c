b9b9c4df26991d61fffe8757558e57b7fdf79801
pinn99k <minn90311@gmail.com>
2026-09-15 14:53:05 +0900

    이진트리 04

--- week4/data_structures_docker/Data-Structures/Binary_Tree/Q4_E_BT.c
+++ week4/data_structures_docker/Data-Structures/Binary_Tree/Q4_E_BT.c
@@ -103,7 +103,14 @@ int main()
 int sumOfOddNodes(BTNode *node)
 
 {
-    /* add your code here */
+    if (node==NULL){
+        return  0;
+    }
+    if ((node->item % 2) != 0 ){
+        //홀수이면 아이템을 리턴
+        return (node->item + sumOfOddNodes(node->left) + sumOfOddNodes(node->right));
+    }
+    return (sumOfOddNodes(node->left) + sumOfOddNodes(node->right));
 }
 
 //////////////////////////////////////////////////////////////////////////////////
